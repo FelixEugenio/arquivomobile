9390c7472919dc2f22afddeeab6bc9ed3170007b
felix mavila <felixmacarenco@gmail.com>
2024-02-29 19:27:04 +0000

    feat: create login feature

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,88 +1,115 @@
-import React, {useState, createContext, ReactNode } from 'react';
-
+import React, { useState, createContext, ReactNode, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { api } from '../services/api';
-
 
 type AuthContextData = {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
-}
+  loadingAuth: boolean;
+  loading: boolean;
+  signOut: () => Promise<void>;
+};
 
 type UserProps = {
   id: string;
   name: string;
   email: string;
-  token: string
-}
-
+  token: string;
+};
 
 type AuthProviderProps = {
   children: ReactNode;
-}
+};
 
 type SignInProps = {
   email: string;
   password: string;
-}
+};
 
 export const AuthContext = createContext({} as AuthContextData);
 
-export function AuthProvider({children}: AuthProviderProps){
+export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>({
     id: '',
     name: '',
     email: '',
-    token: ''
-  })
+    token: '',
+  });
 
-  const [loadingAuth, setLoadingAuth] = useState(false)
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-  const isAuthenticated = !!user.name; 
+  const isAuthenticated = !!user.name;
 
+  useEffect(() => {
+    async function getUser() {
+      const userInfo = await AsyncStorage.getItem('@arquivoGPT');
+      let hasUser: UserProps = JSON.parse(userInfo || '{}');
 
-  async function signIn({ email, password }: SignInProps){
+      if (Object.keys(hasUser).length > 0) {
+        api.defaults.headers.common['Authorization'] = `Bearer ${hasUser.token}`;
+
+        setUser({
+          id: hasUser.id,
+          name: hasUser.name,
+          email: hasUser.email,
+          token: hasUser.token,
+        });
+      }
+
+      setLoading(false);
+    }
+
+    getUser();
+  }, []);
+
+  async function signIn({ email, password }: SignInProps) {
     setLoadingAuth(true);
 
-    try{
+    try {
       const response = await api.post('/session', {
         email,
-        password
-      })
-      //console.log(response.data);
+        password,
+      });
 
       const { id, name, token } = response.data;
 
       const data = {
-        ...response.data
+        ...response.data,
       };
 
-      await AsyncStorage.setItem('@arquivoGPT', JSON.stringify(data))
+      await AsyncStorage.setItem('@arquivoGPT', JSON.stringify(data));
 
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       setUser({
         id,
         name,
         email,
         token,
-      })
+      });
 
       setLoadingAuth(false);
-
-
-    }catch(err){
-      console.log('erro ao acessar', err)
+    } catch (err) {
+      console.log('erro ao acessar', err);
       setLoadingAuth(false);
     }
-
   }
 
-  return(
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+  async function signOut() {
+    await AsyncStorage.clear();
+    setUser({
+      id: '',
+      name: '',
+      email: '',
+      token: '',
+    });
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, loading, loadingAuth, signOut }}>
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,13 @@
-import { View , Text} from "react-native";
+import { View , Text, Button} from "react-native";
+import { AuthContext } from "../../contexts/AuthContext";
+import { useContext } from "react";
 
 export default function Dashboard(){
+    const {signOut} = useContext(AuthContext)
     return(
         <View>
             <Text>Dashboard</Text>
+            <Button title="Sair" onPress={signOut}/>
         </View>
     )
 }
